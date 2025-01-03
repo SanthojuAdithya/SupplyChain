89ac962310040daf0859bfae9827a06966be8797
username <wecp-dummy@gmail.com>
2025-01-03 14:14:24 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/ProductServiceImplJdbc.java
@@ -1,36 +1,42 @@
 package com.wecp.progressive.service.impl;
 
-import java.util.List;
-
 import com.wecp.progressive.dao.ProductDAO;
 import com.wecp.progressive.entity.Product;
 import com.wecp.progressive.service.ProductService;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class ProductServiceImplJdbc implements ProductService {
 
-    @Override
-    public List<Product> getAllProducts() {
-     return  List.of();
+    private ProductDAO productDAO;
+
+    public ProductServiceImplJdbc(ProductDAO productDAO) {
+        this.productDAO = productDAO;
     }
 
     @Override
-    public Product getProductById(int productId) {
-        return null;
+    public List<Product> getAllProducts() throws SQLException {
+        return productDAO.getAllProducts();
     }
 
     @Override
-    public int addProduct(Product product) {
-       return -1;
+    public Product getProductById(int productId) throws SQLException {
+        return productDAO.getProductById(productId);
     }
 
     @Override
-    public void updateProduct(Product product) {
-       
+    public int addProduct(Product product) throws SQLException {
+        return productDAO.addProduct(product);
     }
 
     @Override
-    public void deleteProduct(int productId) {
-       
+    public void updateProduct(Product product) throws SQLException {
+        productDAO.updateProduct(product);
     }
 
+    @Override
+    public void deleteProduct(int productId) throws SQLException {
+        productDAO.deleteProduct(productId);
+    }
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplArraylist.java
@@ -1,17 +1,19 @@
 package com.wecp.progressive.service.impl;
 
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.service.WarehouseService;
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import com.wecp.progressive.entity.Warehouse;
-import com.wecp.progressive.service.WarehouseService;
+@Service
+public class WarehouseServiceImplArraylist implements WarehouseService {
 
-public class WarehouseServiceImplArraylist implements WarehouseService  {
+    private static List<Warehouse> warehouseList = new ArrayList<>();
 
-    private static List<Warehouse> warehouseList;
     @Override
     public List<Warehouse> getAllWarehouses() {
         return warehouseList;
@@ -20,19 +22,18 @@ public class WarehouseServiceImplArraylist implements WarehouseService  {
     @Override
     public int addWarehouse(Warehouse warehouse) {
         warehouseList.add(warehouse);
-        return warehouse.getCapacity();
+        return warehouseList.size();
     }
 
     @Override
     public List<Warehouse> getWarehousesSortedByCapacity() {
-        // Collections.sort(warehouseList);
-        // return warehouseList;
-        warehouseList.sort(Comparator.comparingInt(Warehouse::getCapacity));
-        return warehouseList;
+        List<Warehouse> sortedWarehouses = warehouseList;
+        sortedWarehouses.sort(Comparator.comparing(Warehouse::getCapacity)); // Sort by supplier name
+        return sortedWarehouses;
     }
 
-   @Override 
-     public void emptyArrayList() {
-        warehouseList=new ArrayList<>();
+    @Override
+    public void emptyArrayList() {
+        warehouseList = new ArrayList<>();
     }
 }
